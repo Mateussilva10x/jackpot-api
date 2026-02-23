d4b336ae138f9a35ce39168eb3e62f0e363ade93
Mateus Gonçalves Silva <mateussilva651@gmail.com>
2026-02-23 14:44:35 -0300

    fix: adjusted dockerfile

--- src/test/java/com/worldJackpot/api/controller/StandingsIntegrationTest.java
+++ src/test/java/com/worldJackpot/api/controller/StandingsIntegrationTest.java
@@ -5,7 +5,7 @@ import com.worldJackpot.api.service.StandingsService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
